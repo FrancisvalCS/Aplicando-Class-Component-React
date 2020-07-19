2db440bc4196fdb7d7cd61a8a75cda25337760e7
Francisval Cavalcante <francisvalcs@gmail.com>
2020-07-19 12:07:28 -0300

    Trabalhando com states React Rj

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
 
-/*Constantes*/
-class Equipe extends Component{
+class  App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            nome: 'Matheus', contador: 0
+        }
+        this.aumentar = this.aumentar.bind(this);
+        this.diminuir = this.diminuir.bind(this);
+    }
+
+    aumentar(){
+        let state = this.state;
+        state.contador += 1;
+        this.setState(state);
+    }
+
+    diminuir(){
+        let state = this.state;
+        state.contador -= 1;
+        this.setState(state);
+    }
     render(){
         return(
             <div>
-                <h2>Olá sou o(a) {this.props.nome}</h2>
-                <h3>Cargo: {this.props.cargo}</h3>
-                <h3>idade: {this.props.idade}</h3>
+                <h1>Contador</h1>
+                <h3>
+                    <button onClick={this.diminuir}>-</button> 
+                        {this.state.contador}
+                    <button onClick={this.aumentar}>+</button>
+                </h3>
             </div>
         );
     }
-}
-
-/*Funcção*/
-function App(){
-    return (
-        <div>
-            <h1>Conheça nossa equipe:</h1>
-            <Equipe nome="Metheus" cargo="Programador" idade="24"/>
-        </div>
-    );
 }
 
 export default App;
